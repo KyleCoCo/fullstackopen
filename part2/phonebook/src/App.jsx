050d8d3bbc4f5597d55a9ffabc698149100de17c
lizk_racknerd <lizongkai94@gmail.com>
2025-03-18 17:33:40 +0800

    add promise

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,15 +1,12 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
 
+
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [filterName, setFilterName] = useState('')
   const [newPerson, setNewPerson] = useState(
     {
@@ -18,6 +15,17 @@ const App = () => {
     }
   )
 
+  const hook = () => {
+    console.log('effect')
+    axios
+      .get('http://bitcoin.linkpc.net:3000/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }
+  
+  useEffect(hook, [])
   
   return (
     <div>
